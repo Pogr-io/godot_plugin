dc2faec1055f69d4b86ad858d9f847e2c1cca704
Yannik <80621863+vaporvee@users.noreply.github.com>
2023-05-31 21:22:51 +0200

    plugin environment builds and works correctly

--- src/register_types.cpp
+++ src/register_types.cpp
@@ -3,18 +3,17 @@
 #include <gdextension_interface.h>
 #include <godot_cpp/core/class_db.hpp>
 #include <godot_cpp/core/defs.hpp>
-#include <godot_cpp/godot.hpp>
 #include <godot_cpp/classes/engine.hpp>
+#include <godot_cpp/godot.hpp>
 
 #include "pogr_plugin.h"
-
 using namespace godot;
 
 static pogr_plugin *pogrplugin;
 
-void initialize_pogrplugin_module(ModuleInitializationLevel p_level)
+void gdextension_initialize(ModuleInitializationLevel p_level)
 {
-    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE)
+    if (p_level == MODULE_INITIALIZATION_LEVEL_SCENE)
     {
         ClassDB::register_class<pogr_plugin>();
         pogrplugin = memnew(pogr_plugin);
@@ -22,9 +21,9 @@ void initialize_pogrplugin_module(ModuleInitializationLevel p_level)
     }
 }
 
-void uninitialize_pogrplugin_module(ModuleInitializationLevel p_level)
+void gdextension_terminate(ModuleInitializationLevel p_level)
 {
-    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE)
+    if (p_level == MODULE_INITIALIZATION_LEVEL_SCENE)
     {
         Engine::get_singleton()->unregister_singleton("PogrSDK");
         memdelete(pogrplugin);
@@ -37,8 +36,8 @@ extern "C"
     {
         godot::GDExtensionBinding::InitObject init_obj(p_interface, p_library, r_initialization);
 
-        init_obj.register_initializer(initialize_pogrplugin_module);
-        init_obj.register_terminator(uninitialize_pogrplugin_module);
+        init_obj.register_initializer(gdextension_initialize);
+        init_obj.register_terminator(gdextension_terminate);
         init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
 
         return init_obj.init();
--- src/register_types.h
+++ src/register_types.h
@@ -1,11 +1,7 @@
 #ifndef REGISTER_TYPES_H
 #define REGISTER_TYPES_H
 
-#include <godot_cpp/core/class_db.hpp>
-
-using namespace godot;
-
-void initialize_pogrplugin_module(ModuleInitializationLevel p_level);
-void uninitialize_pogrplugin_module(ModuleInitializationLevel p_level);
+void initialize_pogrplugin_module();
+void uninitialize_pogrplugin_module();
 
 #endif
