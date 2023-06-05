838c4d01b6543b1eabf87f7faa0677bc1fcfb385
Yannik <80621863+vaporvee@users.noreply.github.com>
2023-06-05 18:52:10 +0200

    improved enable window

--- project/addons/pogr_plugin/plugin.gd
+++ project/addons/pogr_plugin/plugin.gd
@@ -1,6 +1,7 @@
 @tool
 extends EditorPlugin
 
+var pogrwindow: Node
 
 func _enable_plugin() -> void:
 	add_autoload_singleton("POGR_Manager","res://addons/pogr_plugin/pogr_manager.tscn")
@@ -10,14 +11,20 @@ func _enable_plugin() -> void:
 	ProjectSettings.set_initial_value("pogr_sdk/ids/build_id", "")
 	ProjectSettings.set("pogr_sdk/api/url","")
 	ProjectSettings.set_initial_value("pogr_sdk/api/url", "")
-	add_control_to_container(EditorPlugin.CONTAINER_TOOLBAR,preload("res://addons/pogr_plugin/plugin_window.tscn").instantiate())
-	
+	pogrwindow = preload("res://addons/pogr_plugin/plugin_window.tscn").instantiate()
+	add_control_to_container(EditorPlugin.CONTAINER_CANVAS_EDITOR_MENU,pogrwindow)
+	add_control_to_container(EditorPlugin.CONTAINER_PROJECT_SETTING_TAB_LEFT,preload("res://addons/pogr_plugin/pogr_credits.tscn").instantiate())
+
 func _disable_plugin() -> void:
 	remove_autoload_singleton("POGR_Manager")
 	ProjectSettings.clear("pogr_sdk/ids/client_id")
 	ProjectSettings.clear("pogr_sdk/ids/build_id")
 	ProjectSettings.clear("pogr_sdk/api/url")
 	print("POGR SDK Plugin got diasbled (Please restart the editor if it's causing issues)")
+	remove_control_from_container(EditorPlugin.CONTAINER_CANVAS_EDITOR_MENU,pogrwindow)
 
 func _get_plugin_name():
 	return "Pogr SDK Plugin"
+
+func _forward_canvas_gui_input(event):
+	print(event)
