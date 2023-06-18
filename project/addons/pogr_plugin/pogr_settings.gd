6607fff32512f63e27e9d10d7e00c3d0aab63c20
Yannik <80621863+vaporvee@users.noreply.github.com>
2023-06-18 18:06:29 +0200

    custom ui bugfixes

--- project/addons/pogr_plugin/pogr_settings.gd
+++ project/addons/pogr_plugin/pogr_settings.gd
@@ -4,20 +4,20 @@ extends Control
 var config: ConfigFile = ConfigFile.new()
 
 func _enter_tree():
-	config.load("res://addons/pogr_plugin/config/pogr.cfg")
-	$Panel/MarginContainer/GridContainer/ClientID.text = config.get_value("api","client_id","")
-	$Panel/MarginContainer/GridContainer/BuildID.text = config.get_value("api","build_id","")
-	$Panel/MarginContainer/GridContainer/ApiURL.text = config.get_value("api","api_url","")
+	config.load("res://addons/pogr_plugin/pogr.cfg")
+	$Panel/MarginContainer/GridContainer/ClientID.text = str(config.get_value("api","client_id"))
+	$Panel/MarginContainer/GridContainer/BuildID.text = str(config.get_value("api","build_id"))
+	$Panel/MarginContainer/GridContainer/ApiURL.text = str(config.get_value("api","api_url"))
 
 func _on_client_id_text_changed(new_text):
 	config.set_value("api","client_id",new_text)
-	config.save("res://addons/pogr_plugin/config/pogr.cfg")
+	config.save("res://addons/pogr_plugin/pogr.cfg")
 func _on_build_id_text_changed(new_text):
 	config.set_value("api","build_id",new_text)
-	config.save("res://addons/pogr_plugin/config/pogr.cfg")
+	config.save("res://addons/pogr_plugin/pogr.cfg")
 func _on_api_url_text_changed(new_text):
 	config.set_value("api","api_url",new_text)
-	config.save("res://addons/pogr_plugin/config/pogr.cfg")
+	config.save("res://addons/pogr_plugin/pogr.cfg")
 
 
 func _on_about_pressed():
